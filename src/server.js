c4f468ecbd2eac99df922a8b2a7d8d53695cc4fe
Bui Viet Anh <buivietanh399@gmail.com>
2022-12-16 10:28:15 +0700

    init project

--- src/server.js
+++ src/server.js
@@ -1,12 +1,20 @@
-const express = require('express') //import
-const path = require('path') //import
+const express = require('express'); //import
+const path = require('path'); //import
+require('dotenv').config();
+
+
 const app = express() //app express
-const port = 3000  //port
-const hostname = "localhost"
+const port = process.env.port || 8000;   //port
+const hostname = process.env.HOST_NAME;  //hostname
 
 //config template engine
-app.set('views', path.join(__dirname,'views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+
+
+//config static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Khai báo route
 app.get('/', (req,res) => { 
